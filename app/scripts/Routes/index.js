15813e99d2611ca2b86bb7de0dcd40725bc795ea
Jesús Gallego Irles <jesusgallego@outlook.com>
2015-12-01 00:23:09 +0100

    Nueva serie
    Se pueden añadir nuevas series pulsando el botón '+'

--- app/scripts/Routes/index.js
+++ app/scripts/Routes/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Router, Route, IndexRoute, HistoryLocation} from 'react-router';
 import Series from '../Components/Series/SeriesBox';
 import Serie from '../Components/Series/Serie';
+import SerieForm from '../Components/Series/SerieForm';
 import Noticias from '../Components/Noticias/NoticiasBox';
 import Noticia from '../Components/Noticias/Noticia';
 import Register from '../Components/Login/Register';
@@ -11,6 +12,7 @@ export default (
           <Route component="App" path='/'>
            <IndexRoute component={Series} />
            <Route path="series" component={Series} />
+           <Route path="nueva_serie" component={SerieForm} />
            <Route path="serie/:id" component={Serie} />
            <Route path="noticias/:section" component={Noticias}/>
            <Route path="noticia/:id" component={Noticia}/>
