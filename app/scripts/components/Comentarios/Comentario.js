ed808a0850790381ee7b62e7221950b71fed6f6b
Jesús Gallego Irles <jesusgallego@outlook.com>
2015-12-06 12:42:01 +0100

    DELETE Y UPDATE Comentarios
    Añadido también cambio visual de los comentarios

--- app/scripts/components/Comentarios/Comentario.js
+++ app/scripts/components/Comentarios/Comentario.js
@@ -1,20 +1,70 @@
 import React, { PropTypes } from 'react';
+import ActionButton from '../ActionButton';
+import ComentariosForm from './ComentariosForm';
 import marked from 'marked';
+import moment from 'moment';
 
 class Comentario extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            edditing: false
+        }
+    }
     rawMarkup () {
         var markup = marked(this.props.children.toString(), {sanitize: true});
         return { __html : markup};
     }
-
+    editMode() {
+        this.setState({
+            edditing: true
+        });
+    }
+    handleCommentEdited(comment) {
+        comment.id = this.props.id;
+        this.props.onCommentEdited(comment);
+        this.setState({
+            edditing: false 
+        });
+    }
+    handleCommentDeleted() {
+        this.props.onCommentDeleted({id: this.props.id});
+    }
+    renderText() {
+        return (
+            <div className="panel-body" dangerouslySetInnerHTML={this.rawMarkup()}></div>
+        );
+    }
+    renderEdit() {
+        return(
+            <ComentariosForm onCommentSubmit={this.handleCommentEdited.bind(this)}>{this.props.children}</ComentariosForm>
+        );
+    }
     render() {
         return (
-            <div className="comment">
-                <h3 className="commentAuthor">
-                  {this.props.author}
-                </h3>
-                <span dangerouslySetInnerHTML={this.rawMarkup()} />
-            </div>
+                <div className="row">
+                    <div className="col-sm-2">
+                        <div className="thumbnail">
+                            <img className="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+                        </div>
+                    </div>
+                    <div className="col-sm-10">
+                        <div className="panel panel-default actions-list">
+                            <div className="panel-heading">
+                                <strong>{this.props.author}</strong> <span className="text-muted">commented {moment(this.props.updatedAt).fromNow()}</span>
+                                    <div className="actions">
+                                        <ActionButton>
+                                            <ActionButton.Item onClick={ this.handleCommentDeleted.bind(this) }>Borrar</ActionButton.Item>
+                                            <ActionButton.Item onClick={ this.editMode.bind(this) }>Editar</ActionButton.Item>
+                                        </ActionButton>
+                                    </div>
+                            </div>
+                            {
+                                this.state.edditing ? this.renderEdit() : this.renderText()
+                            }
+                        </div>
+                    </div>
+                </div>
         );
     }
 }
--- app/scripts/components/Comentarios/ComentariosList.js
+++ app/scripts/components/Comentarios/ComentariosList.js
@@ -5,11 +5,11 @@ class ComentariosList extends React.Component {
     render () {
         var CommentNodes = this.props.data.map(function (comment) {
             return (
-                <Comment author={comment.UsuarioId} key={comment.id}>
+                <Comment author={comment.UsuarioId} id={comment.id} onCommentEdited={this.props.onCommentEdited} onCommentDeleted={this.props.onCommentDeleted} updatedAt={comment.updatedAt} key={comment.id}>
                     {comment.comment}
                 </Comment>
             );
-        })
+        }.bind(this));
         return (
             <div className="commentList">
                 {CommentNodes}
